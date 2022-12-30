6c58726afbdcadc2bdeb313acb6be98b35f3ae58
Maciej Plewko <maciej.plewko@gmail.com>
2022-12-30 19:13:35 +0100

    [ADD] Authorization service

---
+++ authentication-service/cmd/api/handlers.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func (app *Config) Authenticate(w http.ResponseWriter, r *http.Request) {
+	var requestPayload struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	err := app.readJSON(w, r, &requestPayload)
+	if err != nil {
+		log.Println("error reading request payload")
+		app.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+
+	// validate the email and password
+	user, err := app.Models.User.GetByEmail(requestPayload.Email)
+	if err != nil {
+		log.Println("error getting user")
+		app.errorJSON(w, errors.New("user does not exist"), http.StatusBadRequest)
+		return
+	}
+
+	valid, err := user.PasswordMatches(requestPayload.Password)
+	if err != nil || !valid {
+		log.Println("error checking password")
+		app.errorJSON(w, errors.New("invalid credentials"), http.StatusBadRequest)
+		return
+	}
+
+	payload := jsonResponse{
+		Error:   false,
+		Message: fmt.Sprintf("Logged in user %s", user.Email),
+		Data:    user,
+	}
+
+	app.writeJSON(w, http.StatusAccepted, payload)
+}
--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module go-micro
 
 go 1.19
-
-require golang.org/x/crypto v0.4.0
